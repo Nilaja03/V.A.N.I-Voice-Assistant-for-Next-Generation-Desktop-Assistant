6c8e6c42bd3d02e3b9afe014f7fe175bc8f0c0d6
Nilanjana Jamindar <88586459+Nilaja03@users.noreply.github.com>
2025-06-07 12:46:51 +0530

    Add files via upload

---
+++ VANI/engine/features.py
@@ -0,0 +1,169 @@
+import os
+from shlex import quote
+import sqlite3
+import struct
+import subprocess
+import time
+import webbrowser
+from playsound import playsound
+import eel
+import pvporcupine
+import pyaudio
+import pyautogui
+from engine.command import speak
+from engine.config import ASSISTANT_NAME
+import pywhatkit as kit
+from engine.helper import extract_yt_term, remove_words
+from hugchat import hugchat
+
+conn = sqlite3.connect("vani.db")
+cursor = conn.cursor()
+
+# Playing assistant sound function
+@eel.expose
+def playAssistantSound():
+    music_dir = "www\\assets\\audio\\start_sound.mp3"
+    playsound(music_dir)
+
+def openCommand(query):
+    query = query.replace(ASSISTANT_NAME, "")
+    query = query.replace("open", "")
+    query.lower()
+    
+    app_name = query.strip()
+    if app_name != "":
+        try:
+            cursor.execute(
+                'SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+            results = cursor.fetchall()
+
+            if len(results) != 0:
+                speak("Opening "+query)
+                os.startfile(results[0][0])
+            
+            elif len(results) == 0:
+                cursor.execute(
+                    'SELECT url FROM web_command WHERE name IN (?)', (app_name,))
+                results = cursor.fetchall()
+                if len(results) !=0:
+                    speak("Opening "+query)
+                    webbrowser.open(results[0][0])
+                else:
+                    speak("Opening "+query)
+                    try:
+                        os.system('start '+query)
+                    except:
+                        speak("Not Found")
+        except:
+            speak("Something went wrong")
+
+
+
+def PlayYoutube(query):
+    search_term = extract_yt_term(query)
+    speak("Playing "+search_term+" on Youtube")
+    kit.playonyt(search_term)
+
+
+
+# Hot-word Detection
+def hotword():
+    porcupine = None
+    paud = None
+    audio_stream = None
+    try:
+        # pre-trained keywords
+        porcupine = pvporcupine.create(keywords=["jarvis","alexa"])
+        paud = pyaudio.PyAudio()
+        audio_stream = paud.open(rate=porcupine.sample_rate,channels=1,format=pyaudio.paInt16,input=True,frames_per_buffer=porcupine.frame_length)
+
+        # loop for streaming
+        while True:
+            keyword = audio_stream.read(porcupine.frame_length)
+            keyword = struct.unpack_from("h"*porcupine.frame_length,keyword)
+            # processing keyword coming from mic
+            keyword_index = porcupine.process(keyword)
+            # checking first keyword is detected or not
+            if keyword_index>=0:
+                print("Hotword Detected")
+                #pressing shortcut key win+j
+                import pyautogui as autogui
+                autogui.keyDown("win")
+                autogui.press("j")
+                time.sleep(2)
+                autogui.keyUp("win")
+    except:
+        if porcupine is not None:
+            porcupine.delete()
+        if audio_stream is not None:
+            audio_stream.close()
+        if paud is not None:
+            paud.terminate()
+
+
+
+# Find Contacts
+def findContact(query):
+    words_to_remove = [ASSISTANT_NAME, 'make', 'a', 'to', 'phone', 'call', 'send', 'message', 'whatsapp', 'video']
+    query = remove_words(query, words_to_remove)
+    try:
+        query = query.strip().lower()
+        cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+        results = cursor.fetchall()
+        print(results[0][0])
+        mobile_number_str = str(results[0][0])
+        if not mobile_number_str.startswith('+91'):
+            mobile_number_str = '+91' + mobile_number_str
+        return mobile_number_str, query
+    except:
+        speak('Does not exist in contacts')
+        return 0, 0
+
+
+
+# Send Message to Contact via Whatsapp
+def whatsApp(mobile_no, message, flag, name):
+    if flag == 'message':
+        target_tab = 12
+        alexa_message = "Message sent successfully to "+name
+    elif flag == 'call':
+        target_tab = 7
+        alexa_message = "Calling "+name
+    else:
+        target_tab = 6
+        message = ''
+        alexa_message = "Starting video call with "+name
+    
+    # Encode the message for URL
+    encoded_message = quote(message)
+
+    # Construct the URL
+    whatsapp_url = f"whatsapp://send?phone={mobile_no}&text={encoded_message}"
+    
+    # Construct the full command
+    full_command = f'start "" "{whatsapp_url}"'
+
+    # Open WhatsApp with the constructed URL using cmd.exe
+    subprocess.run(full_command, shell=True)
+    time.sleep(5)
+    subprocess.run(full_command, shell=True)
+    time.sleep(5)
+
+    pyautogui.hotkey('ctrl', 'f')
+
+    for i in range(1, target_tab):
+        pyautogui.hotkey('tab')
+    
+    pyautogui.hotkey('enter')
+    speak(alexa_message)
+
+# Hugging face chat
+def chatBot(query):
+    user_input = query.lower()
+    chatbot = hugchat.ChatBot(cookie_path="engine/cookies.json")
+    id = chatbot.new_conversation()
+    chatbot.change_conversation(id)
+    response = chatbot.chat(user_input)
+    print(response)
+    speak(response)
+    return response
